61f99ce12e457b9c100ddb93839d42ce146e8f90
Alex <70447740+AlexTomes007@users.noreply.github.com>
2022-02-14 14:34:39 -0600

    Gui-work

--- CS390-WritingCenter/WritingCenterForms/Program.cs
+++ CS390-WritingCenter/WritingCenterForms/Program.cs
@@ -16,7 +16,7 @@ namespace WritingCenterForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WritingCenterScheduler());
         }
     }
 }
---
+++ CS390-WritingCenter/WritingCenterForms/WritingCenterScheduler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WritingCenterForms
+{
+    public partial class WritingCenterScheduler : Form
+    {
+        public WritingCenterScheduler()
+        {
+            InitializeComponent();
+        }
+
+        private void logIn_Click(object sender, EventArgs e)
+        {
+            ///
+            /// User verification goes here
+            /// for now change which page you want here
+            ///
+            ///userLand1.Show();
+            
+            adminLand1.Show();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adminLand1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ManageSched_Click(object sender, EventArgs e)
+        {
+            adminLand1.Hide();
+            schedulePage1.Show();
+        }
+    }
+}

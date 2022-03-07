fc94fb3fe268cea39d99dbac43af2ef5398b38ad
Alex <70447740+AlexTomes007@users.noreply.github.com>
2022-03-07 14:23:47 -0600

    hid database button

--- CS390-WritingCenter/WritingCenterForms/WCSchedulerForm.Designer.cs
+++ CS390-WritingCenter/WritingCenterForms/WCSchedulerForm.Designer.cs
@@ -44,9 +44,10 @@
             // 
             this.logIn.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.logIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.logIn.Location = new System.Drawing.Point(521, 541);
+            this.logIn.Location = new System.Drawing.Point(391, 440);
+            this.logIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.logIn.Name = "logIn";
-            this.logIn.Size = new System.Drawing.Size(110, 52);
+            this.logIn.Size = new System.Drawing.Size(82, 42);
             this.logIn.TabIndex = 3;
             this.logIn.Text = "Log In";
             this.logIn.UseVisualStyleBackColor = true;
@@ -56,9 +57,10 @@
             // 
             this.password.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.password.Location = new System.Drawing.Point(427, 486);
+            this.password.Location = new System.Drawing.Point(320, 395);
+            this.password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(303, 24);
+            this.password.Size = new System.Drawing.Size(228, 21);
             this.password.TabIndex = 2;
             this.password.Text = "Password";
             this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
@@ -69,9 +71,10 @@
             // 
             this.username.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.username.Location = new System.Drawing.Point(427, 446);
+            this.username.Location = new System.Drawing.Point(320, 362);
+            this.username.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(303, 24);
+            this.username.Size = new System.Drawing.Size(228, 21);
             this.username.TabIndex = 1;
             this.username.Text = "Username";
             this.username.Enter += new System.EventHandler(this.enterUsernameBox);
@@ -91,8 +94,9 @@
             this.panel1.Controls.Add(this.logIn);
             this.panel1.Controls.Add(this.password);
             this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1156, 855);
+            this.panel1.Size = new System.Drawing.Size(867, 695);
             this.panel1.TabIndex = 9;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.Leave += new System.EventHandler(this.resetLogin);
@@ -101,9 +105,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::WritingCenterForms.Properties.Resources.CWC_logo_Black_Primary;
-            this.pictureBox1.Location = new System.Drawing.Point(248, 90);
+            this.pictureBox1.Location = new System.Drawing.Point(186, 73);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(664, 314);
+            this.pictureBox1.Size = new System.Drawing.Size(498, 255);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -111,22 +116,22 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1026, 19);
+            this.button1.Location = new System.Drawing.Point(770, 15);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 46);
+            this.button1.Size = new System.Drawing.Size(93, 37);
             this.button1.TabIndex = 6;
             this.button1.Text = "Database";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.DatabaseDebug_Click);
             // 
             // forgotPasswordButton
             // 
             this.forgotPasswordButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.forgotPasswordButton.Location = new System.Drawing.Point(16, 674);
-            this.forgotPasswordButton.Margin = new System.Windows.Forms.Padding(4);
+            this.forgotPasswordButton.Location = new System.Drawing.Point(12, 548);
             this.forgotPasswordButton.Name = "forgotPasswordButton";
-            this.forgotPasswordButton.Size = new System.Drawing.Size(117, 55);
+            this.forgotPasswordButton.Size = new System.Drawing.Size(88, 45);
             this.forgotPasswordButton.TabIndex = 4;
             this.forgotPasswordButton.Text = "Forgot Password";
             this.forgotPasswordButton.UseVisualStyleBackColor = true;
@@ -134,22 +139,22 @@
             // 
             // bootRight
             // 
-            this.bootRight.Location = new System.Drawing.Point(415, 456);
-            this.bootRight.Margin = new System.Windows.Forms.Padding(4);
+            this.bootRight.Location = new System.Drawing.Point(311, 370);
             this.bootRight.Name = "bootRight";
-            this.bootRight.Size = new System.Drawing.Size(132, 22);
+            this.bootRight.Size = new System.Drawing.Size(100, 20);
             this.bootRight.TabIndex = 6;
             this.bootRight.Text = "test";
             // 
             // WCSchedulerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1155, 741);
+            this.ClientSize = new System.Drawing.Size(866, 602);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.bootRight);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "WCSchedulerForm";
             this.Text = "WC Scheduler";
             this.Load += new System.EventHandler(this.Form1_Load);

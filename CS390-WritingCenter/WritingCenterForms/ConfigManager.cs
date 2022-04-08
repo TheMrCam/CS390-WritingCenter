068dff6682dc35f78b565155813df5cd338b7cb2
TheMrCam <63317483+TheMrCam@users.noreply.github.com>
2022-04-08 14:51:26 -0500

    Started working on Config management

---
+++ CS390-WritingCenter/WritingCenterForms/ConfigManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingCenterForms
+{
+    internal class ConfigManager
+    {
+        public string DatabasePath { get { return ConfigurationManager.ConnectionStrings["DatabaseCSV"].ConnectionString; } }
+        public string SchedulePath { get { return ConfigurationManager.ConnectionStrings["ScheduleCSV"].ConnectionString; } }
+
+
+        private void UpdateConfig(string key, string value, string fileName)
+        //fileName = System.Reflection.Assembly.GetEntryAssembly().Location; //full path + application name
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(fileName);
+            configFile.AppSettings.Settings[key].Value = value;
+
+            configFile.Save();
+        }
+    }
+}
